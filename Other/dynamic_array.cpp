152271957817543b2403eb0856f7c6cd6d811f0a
Derxail <kuzmenko.ta@phystech.edu>
2024-03-14 16:11:28 +0300

    capacity

--- Other/dynamic_array.cpp
+++ Other/dynamic_array.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
+#include <stdexcept>
 
 struct DynamicArray {
     int* arr;
     unsigned size;
+    unsigned capacity;
 
-    DynamicArray(unsigned size = 2) {
+    DynamicArray(unsigned capacity = 1, unsigned size = 1) {
         this->size = size;
-        this->arr = new int[size];
+        this->capacity = capacity;
+        this->arr = new int[capacity];
     }
 
-    void resize(unsigned new_size) {
+    void reserve(unsigned new_capacity) {
         int* old_arr = this->arr;
-        this->arr = new int[new_size];
-        for (unsigned i = 0; i < std::min(this->size, new_size); ++i) {
+        this->arr = new int[new_capacity];
+        if (this->size > new_capacity) {
+            this->size = new_capacity;
+        }
+        for (unsigned i = 0; i < this->size; ++i) {
             this->arr[i] = old_arr[i];
         }
-        this->size = new_size;
+        this->capacity = new_capacity;
         delete[] old_arr;
     }
 
+    void resize(unsigned new_size, int val = 0) {
+        if (new_size < this->size) {
+            this->size = new_size;
+            return;
+        }
+        if (new_size > this->capacity) {
+            this->reserve(new_size);
+        }
+        for (unsigned i = this->size; i < new_size; ++i) {
+            this->arr[i] = val;
+        }
+        this->size = new_size;
+    }
+
     void insert_back(int n) {
-        this->resize(this->size + 1);
+        if (this->size == this->capacity) {
+            this->reserve(2 * this->capacity);
+        }
+        ++this->size;
         this->arr[this->size - 1] = n;
     }
 
     void insert(unsigned idx, int value) {
-        this->resize(this->size + 1);
+        if (this->size == this->capacity) {
+            this->reserve(2 * this->capacity);
+        }
+        ++this->size;
         for (unsigned i = this->size - 1; i > idx; --i) {
             std::swap(this->arr[i], this->arr[i - 1]);
         }
@@ -36,7 +62,7 @@ struct DynamicArray {
         for (unsigned i = idx; i < this->size - 1; ++i) {
             std::swap(this->arr[i], this->arr[i + 1]);
         }
-        this->resize(this->size - 1);
+        --this->size;
     }
 
     void print() {
@@ -47,12 +73,16 @@ struct DynamicArray {
     }
 
     int& operator[](unsigned n) {
+        if (n >= this->size) {
+            std::cout << "Dynamic array index out of range";
+            throw std::out_of_range("Dynamic array index out of range");
+        }
         return this->arr[n];
     }
 };
 
 int main() {
-    DynamicArray arr(5);
+    DynamicArray arr(5, 5);
     arr[0] = 1;
     arr[1] = 2;
     arr[2] = 3;
@@ -64,4 +94,14 @@ int main() {
     arr.print();
     arr.remove(3);
     arr.print();
+    arr.resize(3);
+    arr.print();
+    arr.resize(11, 6);
+    arr.print();
+    arr.reserve(5);
+    arr.print();
+    arr.reserve(8);
+    arr.print();
+    std::cout << arr.size << ' ' << arr.capacity << std::endl;
+    std::cout << arr[11];
 }
