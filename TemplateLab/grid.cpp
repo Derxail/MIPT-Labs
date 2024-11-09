86b3615c5b26353d195f108743409c190fb99b54
Derxail <kuzmenko.ta@phystech.edu>
2024-11-10 02:27:55 +0300

    working

--- TemplateLab/grid.cpp
+++ TemplateLab/grid.cpp
@@ -8,19 +8,20 @@ class Grid final
 public:
     using value_type = T;
     using size_type = unsigned;
-    Grid(Grid<T, dim>&&) = delete;
-    Grid<T, dim>& operator=(Grid<T, dim>&&) = delete;
 private:
     Grid<T, dim - 1>* data;
     size_type size;
-public:
-    Grid(Grid<T, dim - 1> *data , size_type size): data(data) {}
-    template<typename... tail> Grid(size_type size, tail... rest): size{size} {
+    void allocate_memory(size_type size) {
         data = static_cast<Grid<T, dim - 1> *>(operator new[](size * sizeof(Grid<T, dim - 1>)));
+    }
+public:
+    template<typename... tail> Grid(size_type size, tail... rest): size{size} {
+        allocate_memory(size);
         for (auto it = data, end = data + size; it != end; ++it) new(it) Grid<T, dim - 1>(rest...);
     }
     Grid(T const& t): size(1) {
-        data = new Grid<T, dim - 1>(t);
+        allocate_memory(1);
+        new(data) Grid<T, dim - 1>(t);
     }
 
     template<typename... tail> T operator()(size_type idx, tail... rest) const
@@ -53,20 +54,28 @@ public:
 
     ~Grid() {
         for (auto it = data, end = data + size; it != end; ++it) it->~Grid();
-        delete data;
+        //delete[] data;
     }
 public:
     Grid(Grid<T, dim> const& other): size(other.get_size()) {
-        data = static_cast<Grid<T, dim - 1> *>(operator new[](size * sizeof(Grid<T, dim - 1>)));
+        allocate_memory(size);
         for (auto i = 0; i != size; ++i) new(data + i) Grid<T, dim - 1>(other[i]);
     }
     Grid<T, dim>& operator=(Grid<T, dim> const& other) {
-        if (other.get_size() != size) {
-            size = other.get_size();
-            for (auto it = data, end = data + size; it != end; ++it) it->~Grid();
-            data = static_cast<Grid<T, dim - 1> *>(operator new[](size * sizeof(Grid<T, dim - 1>)));
-        }
-        for (auto i = 0; i != size; ++i) data[i] = other[i];
+        Grid<T, dim> tmp(other);
+        // Из функции класса можно получить private member любого другого члена класса... Целый новый мир открылся для меня этой строкой.
+        std::swap(this->data, tmp.data);
+        std::swap(this->size, tmp.size);
+        return *this;
+    }
+    // С новым знанием о private, можно и перемещение написать
+    Grid(Grid<T, dim>&& other): data(other.data), size(other.size()) {
+        other.data = nullptr;
+        other.size = 0;
+    }
+    Grid<T, dim>& operator=(Grid<T, dim>&& other) {
+        std::swap(this->data, other.data);
+        std::swap(this->size, other.size)
         return *this;
     }
 } ;
@@ -77,8 +86,6 @@ class Grid<T, 1> final
 public:
     using value_type = T;
     using size_type = unsigned;
-    Grid(Grid<T, 1>&&) = delete;
-    Grid<T, 1>& operator=(Grid<T, 1>&&) = delete;
 private:
     T* data;
     size_type size;
@@ -126,35 +133,37 @@ public:
 
     ~Grid() {
         delete[] data;
-        std::cout << "Grid 1 destroyed" << std::endl;
     }
 
 public:
     Grid(Grid<T, 1> const& other): Grid(other.get_size()) {
-        for (auto i = 0; i < size; ++i) data[i] = other[i];
+        for (auto i = 0; i != size; ++i) data[i] = other[i];
     }
     Grid<T, 1>& operator=(Grid<T, 1> const& other) {
-        if (other.get_size() != size) {
-            size = other.get_size();
-            this->~Grid();
-            data = new T[size];
-        }
-        for (auto i = 0; i < size; ++i) data[i] = other[i];
+        Grid<T, 1> tmp(other);
+        std::swap(this->data, tmp.data);
+        std::swap(this->size, tmp.size);
         return *this;
     }
-} ;
+    Grid(Grid<T, 1>&& other): data(other.data), size(other.size()) {
+        other.data = nullptr;
+        other.size = 0;
+    }
+    Grid<T, 1>& operator=(Grid<T, 1>&& other) {
+        std::swap(this->data, other.data);
+        std::swap(this->size, other.size)
+        return *this;
+    }
+};
 
 int main() {
     Grid<float, 3> const g3(2, 3, 4, 1.0f);
     assert(1.0f == g3(1, 1, 1));
-    std::cout << "1";
 
     Grid<float, 2> g2(2, 5, 2.0f);
     assert(2.0f == g2(1, 1));
-    std::cout << "2";
 
     g2 = g3[1];
-    std::cout << 3;
-    //assert(1.0f == g2(1, 1));
-    std::cout << "done";
+    assert(1.0f == g2(1, 1));
+    return 0;
 }
